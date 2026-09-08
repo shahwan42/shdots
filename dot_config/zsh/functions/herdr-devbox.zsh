84065ca1ece25d05e39aeaa738993728679ac3d9
Ahmed Shahwan <a.shahwan42@gmail.com>
2026-09-08 13:45:45 +0300

    fix(zsh): default Herdr devbox to as-dev

--- dot_config/zsh/functions/herdr-devbox.zsh
+++ dot_config/zsh/functions/herdr-devbox.zsh
@@ -1,14 +1,12 @@
 # Connect to a local Multipass VM without routing SSH through its Tailnet name.
-# Usage: herdr-devbox <vm-name> [herdr options]
+# Usage: herdr-devbox [vm-name] [herdr options]
 if (( $+commands[multipass] )) && (( $+commands[jq] )); then
   herdr-devbox() {
-    if (( $# < 1 )); then
-      print -u2 'usage: herdr-devbox <vm-name> [herdr options]'
-      return 2
+    local vm_name=as-dev ip
+    if (( $# > 0 )) && [[ $1 != -* ]]; then
+      vm_name=$1
+      shift
     fi
-
-    local vm_name=$1 ip
-    shift
     ip=$(multipass info "$vm_name" --format json 2>/dev/null |
       jq -r --arg vm "$vm_name" '.info[$vm].ipv4[0] // empty') || return 1
     if [[ -z "$ip" ]]; then
